fbe78cef407f500efa09c49816368003a8ddfff2
Weitong Xia <244515121@qq.com>
2016-02-24 11:52:19 +0000

    news_comments

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :links do
+    member do
+      put "like", to: "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+     resources :comments
+  end
+  get 'links'  => 'links#index'
+
+
+  get 'sessions/new'
+
+ 
+
+  resources :users
+  root 'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
+end
